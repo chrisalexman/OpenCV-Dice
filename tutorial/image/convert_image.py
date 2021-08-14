2321549e48a5757c6039fcd516fc4989b3869fca
Chris <christopheralexman@gmail.com>
2021-08-14 14:09:50 -0700

    Add opencv tutorial code for images

---
+++ tutorial/image/convert_image.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import sys
+
+
+def convert_image():
+    img_input_path = "/home/pi/Documents/opencv-dice/tutorial/input/starry_night.jpg"
+    img_output_path = "/home/pi/Documents/opencv-dice/tutorial/output/starry_night.png"
+
+    img = cv.imread(img_input_path)
+
+    if img is None:
+        sys.exit("Could not read the image.")
+
+    cv.imshow("Display window", img)
+
+    k = cv.waitKey(0)
+
+    if k == ord("s"):
+        cv.imwrite(img_output_path, img)
+
+
+if __name__ == '__main__':
+    convert_image()
