a984d114da30d57405d489e84c37ded1dbce5160
Chris <christopheralexman@gmail.com>
2021-08-17 20:51:40 -0700

    Add different edge types

--- find_dice/find_dice.py
+++ find_dice/find_dice.py
@@ -22,11 +22,18 @@ def find_dice():
             break
 
         # Our operations on the frame come here
-        # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        blur = cv.GaussianBlur(gray, (5, 5), 0)
+        edges_wide = cv.Canny(blur, 10, 200)
+        edges_mid = cv.Canny(blur, 30, 150)
+        edges_tight = cv.Canny(blur, 240, 250)
 
         # Display the resulting frame
-        # cv.imshow('frame', gray)
-        cv.imshow('frame', frame)
+        cv.imshow('frame1', edges_wide)
+        cv.imshow('frame2', edges_mid)
+        cv.imshow('frame3', edges_tight)
+
+        # TODO: create trackbar and functions
 
         if cv.waitKey(1) == ord('q'):
             break
