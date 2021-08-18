2e48c8f2bb7215c988ee4a8d695c783728e2901e
Chris <christopheralexman@gmail.com>
2021-08-17 19:42:09 -0700

    Add file to stream video in color, setup of dice recognizer

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='opencv-dice',
     version='1.0',
-    # packages=[''],
+    packages=['find_dice'],
     description='Raspberry Pi 4 2GB + OpenCV to recognize dice rolls.'
 )
